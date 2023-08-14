20b18dcf180d3d5222d2737d18b59c354569647b
elm_niconico <elmgameinfo@gmail.com>
2023-08-15 02:28:33 +0900

    UPDATE: deprecated

--- src/extension/tween/processing.rs
+++
@@ -1,5 +0,0 @@
-use bevy::prelude::Component;
-
-#[doc(hidden)]
-#[derive(Component)]
-pub(crate) struct TweenProcessing(pub(crate) u64);
--- src/processing.rs
+++
@@ -1,5 +0,0 @@
-use bevy::prelude::Component;
-
-/// Undo is ignored while there is one or more `Processing` in the world.
-#[derive(Debug, Default, Eq, PartialEq, Copy, Clone, Hash, Component)]
-pub struct Processing;
